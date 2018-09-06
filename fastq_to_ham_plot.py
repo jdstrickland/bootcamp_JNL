793dba4e3535ee1baf786d0115a64d7eb41a8b40
nwhoppe <nwhoppe@wisc.edu>
2018-09-06 10:23:49 -0700

    added plotting

--- fastq_to_ham_plot.py
+++ fastq_to_ham_plot.py
@@ -4,6 +4,7 @@
 # Script must ultimately be contained in one fluid .py file merged via GitHub
 
 import matplotlib.pyplot as plt
+import itertools
 
 f = open(CTGATC.fastq)
 
@@ -17,5 +18,20 @@ def hamDist(str1, str2):
    diffs = 0
    return diffs
 
+
+
+
+
+
+
+
+
+
 #Make some kind of plot that contains the data you've calculated.
-plt.show()
+seq_list = getSeqs(f)
+f.close()
+hamming_distances = [hamDist(seq1, seq2) for seq1, seq2 in itertools.combinations(seq_list, 2)]
+plt.scatter(range(1, len(hamming_distances) + 1), hamming_distances)
+plt.xlabel('Sequence pair')
+plt.ylabel('Hamming Distance')
+plt.savefig('hamming_plot.png')
